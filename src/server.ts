7ab2d803fe83aed136e02d2ee967fb24be05237c
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2024-04-04 00:56:16 +0100

    folders structured

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(3000, () => {
   console.log("Server running!");
