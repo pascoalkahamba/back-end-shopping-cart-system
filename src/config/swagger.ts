f28c486f3e3146f8c3e7e9b70930d74c219b19b2
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2024-04-04 15:52:41 +0100

    git: conflicts solved

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,36 +1,7 @@
 import { Express } from "express";
-import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
 import swaggerSpec from "../swagger.json";
-
-const options: swaggerJsDoc.Options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Shopping Cart System",
-      version: "1.0.0",
-    },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-
-  apis: ["./src/routes/*.ts"], // files containing annotations as above
-};
-
-// const swaggerSpec = swaggerJsDoc(options);
 
 export function swaggerDocs(app: Express, port: number) {
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
